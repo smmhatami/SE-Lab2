edf993a6c3f2d59f196880ee820619b9edc24839
Seyed Mohammad Mahdi Hatami <hatamik7@gmail.com>
2023-11-05 13:01:00 +0330

    create Class PhoneOrderService

---
+++ src/PaymentServices/PhoneOrderService.java
@@ -0,0 +1,24 @@
+package PaymentServices;
+
+public class PhoneOrderService implements OrderService {
+
+    @Override
+    public void onSiteOrderRegister(String customerName) {
+        //Empty Body
+    }
+
+    @Override
+    public void onlineOrderRegister(String customerName) {
+        //Empty Body
+    }
+
+    @Override
+    public void onSiteOrderPayment(int foodPrice) {
+        //Empty Body
+    }
+
+    @Override
+    public void onlineOrderPayment(int foodPrice) {
+        //Empty Body!
+    }
+}
